bd3ce119df8e0f8083336f1e8e02b56fdefcf3f8
Kathryn <Kathryn@Prometheus>
2018-10-23 18:38:12 -0700

    Updated Cards, Deck, and DeckTest just need to run tomorrow

--- src/ticketToRide/DeckTest.java
+++ src/ticketToRide/DeckTest.java
@@ -11,13 +11,43 @@ public class DeckTest {
 	
 	@Before
 	public void setUp() {
+		
+		System.out.println("Testing the test");
+		TrainCarCard card1 = new TrainCarCard("red");
+		TrainCarCard card2 = new TrainCarCard("red");
+		TrainCarCard card3 = new TrainCarCard("red");
+		TrainCarCard card4 = new TrainCarCard("red");
+		TrainCarCard card5 = new TrainCarCard("blue");
+		
+		DestCard card6 = new DestCard("LA","SF", 5 );
+		DestCard card6 = new DestCard("DC","Boston", 4 );
+		DestCard card6 = new DestCard("ABQ","Seattle", 7 );
+		
 		d1 = new Deck();
-		d2 = d1;
+		d2 = new Deck();
+		
+		d2.add(card1);
+		d2.add(card2);
+		d2.add(card3);
+		d2.add(card4);
+		d2.add(card5);
+		
+	}
+	
+	@Test
+	public void addCards() {
+		d1.add(card1);
+		d1.add(card2);
+		assertTrue(d1.size() > 0);
 	}
 
 	// must be some way to 
 	@Test
 	public void shuffleTest() {
+		d1.add(card3);
+		d1.add(card4);
+		d1.add(card5);
+		assertTrue(d1.equals(d2));
 		assertFalse(d1.equals(d2.shuffle()));
 	}
 
