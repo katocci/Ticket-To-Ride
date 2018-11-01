f533c2eddb7905d8de6c4c927565de0b136a1b9c
Justin <aquino.j94@gmail.com>
2018-11-01 16:10:08 -0700

    Added destinations with the routes

--- src/ticketToRide/GameBoard.java
+++ src/ticketToRide/GameBoard.java
@@ -13,23 +13,35 @@ public class GameBoard {
     private static String[] color = {"RED", "BLUE", "GREEN", "YELLOW", "BLACK", "BLANK"};
     //private Deck tcDeck;
     //private Deck dcDeck;
-    private ArrayList<Routes> gameRoutes;
+    //private HashMap<String[], ArrayList<Routes>> destinations;
+    //private ArrayList<Routes> gameRoutes;
+    private HashMap<String[], Routes> destinations;
     private int min = 1;
     private int max = color.length;
 
 
     public GameBoard() {
-        gameRoutes = new ArrayList<>();
-        gameRoutes.add(new Routes("RED", 4));
-        gameRoutes.add(new Routes("BLACK", 2));
-        gameRoutes.add(new Routes("BLANK", 4));
-        gameRoutes.add(new Routes("RED", 3));
+        /*
+            Using an array for the keys/destinations since the routes are bi-directional.
+            It's the same route regardless which end you start in.
+         */
+        destinations = new HashMap<>();
+        destinations.put(new String[]{"LA", "SF"}, new Routes("RED", 3));
+        destinations.put(new String[]{"SF", "NY"}, new Routes("BLUE", 5));
+        destinations.put(new String[]{"LA", "SD"}, new Routes("BLACK", 2));
+        destinations.put(new String[]{"SF", "LV"}, new Routes("BLANK", 4));
+        destinations.put(new String[]{"NY", "DC"}, new Routes("GREEN", 4));
+
+//        gameRoutes = new ArrayList<>();
+//        gameRoutes.add(new Routes("RED", 4));
+//        gameRoutes.add(new Routes("BLACK", 2));
+//        gameRoutes.add(new Routes("BLANK", 4));
+//        gameRoutes.add(new Routes("RED", 3));
     }
 
     class Routes {
         private int routeLength;
         private String routeColor;
-        private String carTypeNeeded;
 
         /*
              Default constructor is IF the user's want the gameboard to be randomized
@@ -47,10 +59,6 @@ public class GameBoard {
 
         public int getRouteLength() {
             return routeLength;
-        }
-
-        public String getCarTypeNeeded() {
-            return carTypeNeeded;
         }
 
         public String getRouteColor() {
